1c34e45bef77012a117835cdc538cecee451f372
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-06-22 21:20:41 +0200

    delete vardefunc stubs

--- stubs/vardefunc/placebo.pyi
+++
@@ -1,8 +0,0 @@
-import vapoursynth as vs
-from .util import FormatError as FormatError
-from typing import Any, List, Union
-
-core: Any
-
-def deband(clip: vs.VideoNode, radius: float=..., threshold: Union[float, List[float]]=..., iterations: int=..., grain: Union[float, List[float]]=..., chroma: bool=..., **kwargs: Any) -> vs.VideoNode: ...
-def shader(clip: vs.VideoNode, width: int, height: int, shader_file: str, luma_only: bool=..., **kwargs: Any) -> vs.VideoNode: ...
--- stubs/vardefunc/sharp.pyi
+++
@@ -1,7 +0,0 @@
-import vapoursynth as vs
-from .util import FormatError as FormatError
-from typing import Any
-
-core: Any
-
-def z4usm(clip: vs.VideoNode, radius: int=..., strength: float=...) -> vs.VideoNode: ...
