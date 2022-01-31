101ec884c5f3c6ce57561d004a740872c1524871
Ichunjo <ichunjo.le.terrible@gmail.com>
2022-01-31 17:55:51 +0100

    add __all__

--- debandshit/__init__.py
+++ debandshit/__init__.py
@@ -3,3 +3,9 @@
 from .debanders import *
 from .f3kdb import *
 from .placebo import *
+
+__all__ = [
+    'dumb3kdb', 'f3kbilateral', 'f3kpf', 'lfdeband', 'placebo_deband',
+    'SAMPLEMODE', 'F3kdb', 'SampleMode',
+    'Placebo'
+]
