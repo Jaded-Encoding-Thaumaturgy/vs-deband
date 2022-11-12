038a95fbee4af2416617cdeedec87805819629e0
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-11-12 18:31:21 +0100

    Update `Placebo`

--- vsdeband/placebo.py
+++ vsdeband/placebo.py
@@ -65,70 +65,58 @@ class Placebo(Debander):
     iterations: int = 1
     grain: float | list[float] = 6.0
 
-    placebodb_args: dict[str, Any]
+    dither: PlaceboDither = PlaceboDither.DEFAULT
+    renderer: bool = False
 
-    def __init__(self,
-                 radius: float = 16.0, threshold: float | list[float] = 4.0,
-                 iterations: int = 1, grain: float | list[float] = 6.0,
-                 **kwargs: Any) -> None:
+    @inject_self
+    def deband(
+        self, clip: vs.VideoNode, radius: float | None = None, threshold: float | list[float] | None = None,
+        iterations: int | None = None, grain: float | list[float] | None = None, dither: PlaceboDither | None = None,
+        renderer: bool | None = None
+    ) -> vs.VideoNode:
         """
-        Wrapper for placebo.Deband
-        https://github.com/Lypheo/vs-placebo#vs-placebo
+        Main deband function, wrapper for `placebo.Deband <https:/github.com/Lypheo/vs-placebo#vs-placebo>`_
 
+        :param clip:            Source clip
         :param radius:          The debanding filter's initial radius.
                                 The radius increases linearly for each iteration.
                                 A higher radius will find more gradients,
                                 but a lower radius will smooth more aggressively.
-
         :param threshold:       The debanding filter's cut-off threshold.
                                 Higher numbers increase the debanding strength dramatically,
                                 but progressively diminish image details.
-
         :param iterations:      The number of debanding steps to perform per sample.
                                 Each step reduces a bit more banding,
                                 but takes time to compute.
                                 Note that the strength of each step falls off very quickly,
                                 so high numbers (>4) are practically useless.
-
         :param grain:           Add some extra noise to the image.
                                 This significantly helps cover up remaining quantization artifacts.
                                 Higher numbers add more noise.
                                 Note: When debanding HDR sources, even a small amount of grain can result
                                 in a very big change to the brightness level.
                                 It's recommended to either scale this value down or disable it entirely for HDR.
+        :param dither:          Specify what kind of Placebo dithering will be used.
+        :param renderer:        Make libplacebo Deband use the renderer API over the normal API.
 
-        :param kwargs:          Arguments passed to `placebo.Deband`.
-        """
-
-        self.radius = radius
-
-        if isinstance(threshold, (float, int)):
-            self.threshold = [threshold] * 3
-        else:
-            self.threshold = threshold + [threshold[-1]] * (3 - len(threshold))
-
-        self.iterations = iterations
-
-        if isinstance(grain, (float, int)):
-            self.grain = [grain] * 3
-        else:
-            self.grain = grain + [grain[-1]] * (3 - len(grain))
-
-        self.placebodb_args = kwargs
-
-    @inject_self
-    def deband(self, clip: vs.VideoNode) -> vs.VideoNode:
-        """
-        Main deband function
-
-        :param clip:            Source clip
         :return:                Debanded clip
         """
-        if clip.format is None:
-            raise VariableFormatError(self.__class__.deband, 'Variable format not allowed!')
+
+        assert check_variable(clip, self.__class__.deband)
+
+        radius = fallback(radius, self.radius)
+        threshold = normalize_seq(fallback(threshold, self.threshold))
+        iterations = fallback(iterations, self.iterations)
+        grain = normalize_seq(fallback(grain, self.grain))
+        dither = fallback(dither, self.dither)
+        renderer = fallback(renderer, self.renderer)
 
         debs = [
-            p.placebo.Deband(1, self.iterations, thr, self.radius, gra, **self.placebodb_args)
-            for p, thr, gra in zip(split(clip), self.threshold, self.grain)
+            p.placebo.Deband(1, iterations, thr, radius, gra, **dither.placebo_args, renderer_api=renderer)
+            for p, thr, gra in zip(split(clip), threshold, grain)
         ]
-        return debs[0] if len(debs) == 1 else join(debs, clip.format.color_family)
+
+        if len(debs) == 1:
+            return debs[0]
+
+        return join(debs, clip.format.color_family)
