7294afcfbf09a5ca334b1a8a4fd618306030a042
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-11-12 18:30:49 +0100

    Add `PlaceboDither`

--- vsdeband/placebo.py
+++ vsdeband/placebo.py
@@ -1,16 +1,61 @@
 from __future__ import annotations
 
-from typing import Any
-
-from vstools import VariableFormatError, inject_self, join, split, vs
+from dataclasses import dataclass
+from vstools import check_variable, fallback, inject_self, join, normalize_seq, split, vs, CustomIntEnum, KwargsT
 
 from .abstract import Debander
 
 __all__ = [
+    'PlaceboDither',
     'Placebo'
 ]
 
 
+class PlaceboDither(CustomIntEnum):
+    NONE = -1
+    """No dithering."""
+
+    BLUE_NOISE = 0
+    """
+    Dither with blue noise. Very high quality, but requires the use of a
+    LUT. Warning: Computing a blue noise texture with a large size can be
+    very slow, however this only needs to be performed once. Even so, using
+    this with a `lut_size` greater than 6 is generally ill-advised. This is
+    the preferred/default dither method.
+    """
+
+    DEFAULT = BLUE_NOISE
+
+    ORDERED_LU = 1
+    """
+    Dither with an ordered (bayer) dither matrix, using a LUT. Low quality,
+    and since this also uses a LUT, there's generally no advantage to picking
+    this instead of `BLUE_NOISE`. It's mainly there for testing.
+    """
+
+    ORDERED_FIXED = 2
+    """
+    The same as `ORDERED_LUT`, but uses fixed function math instead
+    of a LUT. This is faster, but only supports a fixed dither matrix size
+    of 16x16 (equal to a `lut_size` of 4). Requires GLSL 130+.
+    """
+
+    WHITE_NOISE = 3
+    """
+    Dither with white noise. This does not require a LUT and is fairly cheap
+    to compute. Unlike the other modes it doesn't show any repeating
+    patterns either spatially or temporally, but the downside is that this
+    is visually fairly jarring due to the presence of low frequencies in the
+    noise spectrum. Used as a fallback when the above methods are not
+    available.
+    """
+
+    @property
+    def placebo_args(self) -> KwargsT:
+        """Get arguments you must pass to .placebo.Debander for this dither mode."""
+        if self is PlaceboDither.NONE:
+            return dict(dither=False, dither_algo=0)
+        return dict(dither=True, dither_algo=self.value)
 class Placebo(Debander):
     radius: float
     thrs: list[float]
