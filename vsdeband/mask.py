119d73d4d6237afd9575f31bf643b1d329aa609a
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-11-14 16:30:50 +0100

    Add `deband_detail_mask`

--- vsdeband/mask.py
+++ vsdeband/mask.py
@@ -1,11 +1,41 @@
 from __future__ import annotations
 
-from vsexprtools import aka_expr_available, norm_expr
-from vstools import disallow_variable_format, get_peak_value, get_y, vs, CustomRuntimeError
+from vsexprtools import ExprOp, aka_expr_available, combine, norm_expr
+from vstools import (
+    CustomRuntimeError, core, depth, disallow_variable_format, expect_bits, get_peak_value, get_y, iterate, vs
+)
+from vsrgtools import gauss_blur, removegrain, RemoveGrainModeT, RemoveGrainMode
 
 __all__ = [
+    'deband_detail_mask',
+
     'adg_mask'
 ]
+
+
+def deband_detail_mask(
+    clip: vs.VideoNode,
+    sigma: float = 1.0, rxsigma: list[int] = [50, 200, 350],
+    pf_sigma: float | None = 1.0, brz: tuple[int, int] = (2500, 4500),
+    rg_mode: RemoveGrainModeT = RemoveGrainMode.MINMAX_MEDIAN_OPP
+) -> vs.VideoNode:
+    clip_y, bits = expect_bits(get_y(clip), 16)
+
+    pf = gauss_blur(clip_y, pf_sigma) if pf_sigma else clip_y
+    ret = pf.retinex.MSRCP(rxsigma, None, 0.005)
+
+    blur_ret = gauss_blur(ret, sigma)
+    blur_ret_diff = combine([blur_ret, ret], ExprOp.SUB).std.Deflate()
+    blur_ret_brz = iterate(blur_ret_diff, core.std.Inflate, 4)
+    blur_ret_brz = blur_ret_brz.std.Binarize(brz[0]).morpho.Close(size=8)
+
+    prewitt_mask = clip_y.std.Prewitt().std.Binarize(brz[1]).std.Deflate().std.Inflate()
+    prewitt_brz = prewitt_mask.std.Binarize(brz[1]).morpho.Close(size=4)
+
+    merged = combine([blur_ret_brz, prewitt_brz], ExprOp.ADD)
+    rm_grain = removegrain(merged, rg_mode)
+
+    return depth(rm_grain, bits)
 
 
 @disallow_variable_format
