f9a9a9b3fed8319e0ec4c2237e6f9cd215b61619
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-02-09 17:17:55 +0100

    Rewrite f3kdb wrapper

--- vsdeband/f3kdb.py
+++ vsdeband/f3kdb.py
@@ -1,120 +1,103 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
-from typing import Any, cast
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Any, Literal, overload
 
 from vstools import (
-    CustomIntEnum, CustomValueError, DataType, FuncExceptT, VSFunction, check_variable, clamp_arr, core, fallback,
-    inject_self, normalize_seq, vs
+    ColorRange, ColorRangeT, CustomIntEnum, CustomRuntimeError, FunctionUtil, PlanesT, core, fallback, inject_self,
+    normalize_seq, vs
 )
 
 from .abstract import Debander
 
 __all__ = [
-    'F3kdbPlugin', 'SampleMode',
+    'SampleMode',
 
     'F3kdb'
 ]
 
 
-class SampleMode(CustomIntEnum):
+if TYPE_CHECKING:
+    class Algorithm(CustomIntEnum):
+        OLD = 0
+        UNIFORM = 1
+        GAUSSIAN = 2
+
+        @overload
+        def __call__(  # type: ignore
+            self: Literal[GAUSSIAN], sigma: float = 1.0, grain_sigma: float = 1.0
+        ) -> SampleModeAlgorithmWithInfo:
+            ...
+
+        @overload
+        def __call__(self) -> SampleModeAlgorithmWithInfo:
+            ...
+
+        def __call__(self, *args: Any) -> Any:  # type: ignore
+            ...
+
+
+def _create_Algorithm(sample_mode: SampleMode | SampleModeMidDiffInfo) -> type[Algorithm]:
+    class inner_algorithm(CustomIntEnum):
+        OLD = 0
+        UNIFORM = 1
+        GAUSSIAN = 2
+
+        def __call__(  # type: ignore
+            self: Literal[GAUSSIAN], sigma: float = 1.0, grain_sigma: float = 1.0
+        ) -> SampleModeAlgorithmWithInfo:
+            return SampleModeAlgorithmWithInfo(self, sample_mode, sigma, grain_sigma)  # type: ignore
+
+    return inner_algorithm  # type: ignore
+
+
+class SampleModeBase:
+    if TYPE_CHECKING:
+        Algorithm: type[Algorithm]
+    else:
+        @property
+        def Algorithm(self) -> type[Algorithm]:
+            return _create_Algorithm(self)
+
+
+@dataclass
+class SampleModeMidDiffInfo(SampleModeBase):
+    sample_mode: SampleMode
+    thr_mid: int | list[int]
+    thr_max: int | list[int]
+
+
+@dataclass
+class SampleModeAlgorithmWithInfo:
+    algo: Algorithm
+    sample_mode: SampleMode | SampleModeMidDiffInfo
+    sigma: float = 1.0
+    grain_sigma: float = 1.0
+
+
+class SampleMode(SampleModeBase, CustomIntEnum):
     COLUMN = 1
+    """Take 2 pixels as reference pixel. Reference pixels are in the same column of current pixel."""
+
     SQUARE = 2
+    """Take 4 pixels as reference pixel. Reference pixels are in the square around current pixel."""
+
     ROW = 3
+    """Take 2 pixels as reference pixel. Reference pixels are in the same row of current pixel."""
+
     COL_ROW_MEAN = 4
+    """Arithmetic mean of COLUMN and ROW. Reference points are randomly picked within the range."""
 
-    @property
-    def step(self) -> int:
-        return 16 if self is SampleMode.SQUARE else 32
+    MEAN_DIFF = 5
+    """Similar to COL_ROW_MEAN, adds max/mid diff thresholds."""
+
+    def __call__(  # type: ignore
+        self: Literal[MEAN_DIFF], thr_mid: int | list[int], thr_max: int | list[int]
+    ) -> SampleModeMidDiffInfo:
+        return SampleModeMidDiffInfo(self, thr_mid, thr_max)
 
 
-class F3kdbPlugin(CustomIntEnum):
-    OLD = 0
-    NEO = 1
-    NEO_r8 = 2
-    NEO_r9 = 3
-
-    @property
-    def is_neo(self) -> bool:
-        return self is not F3kdbPlugin.OLD
-
-    def thr_peak(self, full_scale: bool = False) -> int:
-        bits = 8
-
-        if full_scale or self is F3kdbPlugin.NEO_r9:
-            bits += 8
-        elif self is F3kdbPlugin.NEO_r8:
-            bits += 4
-
-        return (1 << bits) - 1
-
-    @property
-    def namespace(self) -> vs.Plugin:
-        return core.neo_f3kdb if self.is_neo else core.f3kdb
-
-    @inject_self.with_args(None)
-    def Deband(
-        self, clip: vs.VideoNode, y: int = 0, cb: int = 0, cr: int = 0, *,
-        range: int | None = None, grainy: int | None = None, grainc: int | None = None,
-        keep_tv_range: bool = True, output_depth: int = 16,
-        seed: int | None = None, sample_mode: int | None = None,
-        dynamic_grain: int | None = None, dither_algo: int | None = None,
-        preset: DataType | None = None, blur_first: int | None = None,
-        random_algo_ref: int | None = None, random_algo_grain: int | None = None,
-        random_param_ref: float | None = None, random_param_grain: float | None = None,
-        full_scale: bool = False
-    ) -> vs.VideoNode:
-        kwargs = dict(
-            range=range, grainy=grainy, grainc=grainc, sample_mode=sample_mode, seed=seed,
-            blur_first=blur_first, dynamic_grain=dynamic_grain, random_param_grain=random_param_grain,
-            keep_tv_range=keep_tv_range, output_depth=output_depth, random_algo_ref=random_algo_ref,
-            random_algo_grain=random_algo_grain, random_param_ref=random_param_ref, preset=preset,
-            dither_algo=dither_algo
-        )
-
-        if full_scale:
-            if self is F3kdbPlugin.NEO_r8:
-                y, cb, cr = max(1, y >> 4), max(1, cb >> 4), max(1, cr >> 4)
-            elif self <= F3kdbPlugin.NEO:
-                y, cb, cr = max(1, y >> 7), max(1, cb >> 7), max(1, cr >> 7)
-        else:
-            if self is F3kdbPlugin.NEO_r9:
-                y, cb, cr = max(1, y << 7), max(1, cb << 7), max(1, cr << 7)
-            elif self is F3kdbPlugin.NEO_r8:
-                y, cb, cr = max(1, y >> 3), max(1, cb >> 3), max(1, cr >> 3)
-
-        if self >= F3kdbPlugin.NEO_r8:
-            kwargs |= dict(y2=y, cb2=cb, cr2=cr)
-        else:
-            kwargs |= dict(y=y, cb=cb, cr=cr)
-
-        return cast(VSFunction, self.namespace.Deband)(clip, **kwargs)
-
-    @classmethod
-    def from_param(cls, use_neo: bool | None) -> F3kdbPlugin:  # type: ignore[override]
-        if use_neo is None:
-            use_neo = hasattr(core, 'neo_f3kdb')
-
-        if not use_neo:
-            return F3kdbPlugin.OLD
-
-        if 'y2' in core.neo_f3kdb.Deband.__signature__.parameters:  # type: ignore[attr-defined]
-            try:
-                core.std.BlankClip(format=vs.YUV420P8).neo_f3kdb.Deband(y2=512)
-                return F3kdbPlugin.NEO_r9
-            except vs.Error:
-                return F3kdbPlugin.NEO_r8
-
-        return F3kdbPlugin.NEO
-
-    def get_sample_step(self, sample_mode: SampleMode, func: FuncExceptT | None = None) -> tuple[SampleMode, int]:
-        if sample_mode > SampleMode.SQUARE and not self.is_neo:
-            raise CustomValueError(
-                'Normal fk3db doesn\'t support SampleMode.ROW or SampleMode.COL_ROW_MEAN',
-                func or self.__class__.get_sample_step
-            )
-
-        return sample_mode, sample_mode.value // 2 if self >= F3kdbPlugin.NEO_r8 else sample_mode.value
+SampleModeT = SampleMode | SampleModeMidDiffInfo | SampleModeAlgorithmWithInfo
 
 
 @dataclass
@@ -125,121 +108,72 @@ class F3kdb(Debander):
     thr: int | list[int] | None = None
     grains: int | list[int] | None = None
 
-    sample_mode: SampleMode | None = None
+    sample_mode: SampleModeT | None = None
 
     seed: int | None = None
     dynamic_grain: int | None = None
-    dither_algo: int | None = None
 
     blur_first: bool | None = None
-
-    use_neo: bool | None = field(default=None, kw_only=True)
-    full_scale: bool = field(default=False, kw_only=True)
 
     def __post_init__(self) -> None:
         super().__post_init__()
 
         self.config = Debander.SupportsConfig(True, True, False)
-        self.plugin = F3kdbPlugin.from_param(self.use_neo)
 
     @inject_self
     def deband(  # type: ignore[override]
         self, clip: vs.VideoNode,
         radius: int = 16,
-        thr: int | list[int] = 30,
+        thr: int | list[int] = 7680,
         grains: int | list[int] = 0,
-        sample_mode: SampleMode = SampleMode.SQUARE,
+        sample_mode: SampleModeT = SampleMode.SQUARE,
         seed: int | None = None,
         dynamic_grain: int | None = None,
-        dither_algo: int | None = None,
-        blur_first: bool | None = None
+        blur_first: bool | None = None,
+        color_range: ColorRangeT | None = None,
+        planes: PlanesT = None
     ) -> vs.VideoNode:
-        """
-        Main deband function
+        if not hasattr(core, 'neo_f3kdb'):
+            raise CustomRuntimeError('You are missing the neo_f3kdb plugin!')
 
-        Handle debanding operations onto a clip using a set of configured parameters.
+        if 'y_2' not in core.neo_f3kdb.Deband.__signature__.parameters:
+            raise CustomRuntimeError('You are using an outdated version of neo_f3kdb, upgrade now!')
 
-        Before neo_f3kdb r7, both f3kdb and neo_f3kdb change their strength
-        at 1 + 16 * n for SampleMode.SQUARE and 1 + 32 * n for SampleMode.COLUMN,
-        SampleMode.ROW or SampleMode.COL_ROW_MEAN.
-
-        This function aimed to average n and n + 1 strength for better debanding accuracy.
-
-        :param radius:          Banding detection range.
-        :param thr:             Banding detection thr(s) for planes.
-        :param grain:           Specifies amount of grains added in the last debanding stage.
-        :param sample_mode:     Valid modes are:
-                                * SampleMode.COLUMN: Take 2 pixels as reference pixel.
-                                  Reference pixels are in the same column of current pixel.
-                                * SampleMode.SQUARE: Take 4 pixels as reference pixel.
-                                  Reference pixels are in the square around current pixel.
-                                * SampleMode.ROW: Take 2 pixels as reference pixel.
-                                  Reference pixels are in the same row of current pixel.
-                                  Only `neo_f3kdb` supports it.
-                                * SampleMode.COL_ROW_MEAN: Arithmetic mean of COLUMN and ROW.
-                                  Reference points are randomly picked within the range.
-                                  Only `neo_f3kdb` supports it.
-
-        :return:                Debanded clip.
-        """
-
-        assert check_variable(clip, self.__class__.deband)
+        func = FunctionUtil(clip, self.deband, planes, (vs.GRAY, vs.YUV), 16)
 
         radius = fallback(self.radius, radius)
 
-        sample_mode, step = self.plugin.get_sample_step(fallback(self.sample_mode, sample_mode), self.__class__.deband)
-
-        thrs = cast(
-            tuple[int, int, int], tuple(clamp_arr(normalize_seq(thr), 1, self.plugin.thr_peak(self.full_scale)))
-        )
+        y, cb, cr = func.norm_seq(fallback(self.thr, thr))
         gry, grc = normalize_seq(fallback(self.grains, grains), 2)
 
-        kwargs = dict[str, Any](
-            range=radius, grainy=gry, grainc=grc, sample_mode=sample_mode,
-            seed=seed if self.seed is None else self.seed,
-            dynamic_grain=dynamic_grain if self.dynamic_grain is None else self.dynamic_grain,
-            dither_algo=dither_algo if self.dither_algo is None else self.dither_algo,
-            blur_first=blur_first if self.blur_first is None else self.blur_first,
-            full_scale=self.full_scale
+        sample_mode = fallback(self.sample_mode, sample_mode)  # type: ignore
+        seed = fallback(self.seed, seed)
+        blur_first = fallback(self.blur_first, blur_first)
+        dynamic_grain = fallback(self.dynamic_grain, dynamic_grain)
+
+        color_range = ColorRange.from_param(
+            color_range, self.deband
+        ) or ColorRange.from_video(func.work_clip, func=self.deband)
+
+        random_algo_ref = 1
+        random_param_ref = random_param_grain = 1.0
+
+        if isinstance(sample_mode, SampleModeAlgorithmWithInfo):
+            random_param_ref, random_param_grain = sample_mode.sigma, sample_mode.grain_sigma
+            random_algo_ref = sample_mode.algo.value
+            sample_mode = sample_mode.sample_mode
+
+        y1 = cb1 = cr1 = y2 = cb2 = cr2 = None
+
+        if isinstance(sample_mode, SampleModeMidDiffInfo):
+            y1, cb1, cr1 = func.norm_seq(sample_mode.thr_mid)
+            y2, cb2, cr2 = func.norm_seq(sample_mode.thr_max)
+            sample_mode = sample_mode.sample_mode
+
+        debanded = core.neo_f3kdb.Deband(
+            clip, radius, y, cb, cr, gry, grc, sample_mode.value, seed, blur_first, dynamic_grain,  # type: ignore
+            None, None, None, color_range.is_limited, 16, random_algo_ref, random_algo_ref,
+            random_param_ref, random_param_grain, None, y1, cb1, cr1, y2, cb2, cr2, True
         )
 
-        if self.plugin >= F3kdbPlugin.NEO_r9 or all(x % step == 1 for x in thrs):
-            return self.plugin.Deband(clip, *thrs, **kwargs)
-
-        lows = cast(tuple[int, int, int], tuple(((th - 1) // step * step + 1 for th in thrs)))
-        highs = cast(tuple[int, int, int], tuple((lo + step for lo in lows)))
-
-        lo_clip = self.plugin.Deband(clip, *lows, **kwargs)
-        hi_clip = self.plugin.Deband(clip, *highs, **kwargs)
-
-        if clip.format.color_family == vs.GRAY:
-            weight = [(thrs[0] - lows[0]) / step]
-        else:
-            weight = [(thr - low) / step for thr, low in zip(thrs, lows)]
-
-        return lo_clip.std.Merge(hi_clip, weight)
-
-    @inject_self
-    def grain(  # type: ignore[override]
-        self, clip: vs.VideoNode, strength: int | tuple[int, int] = 4, dynamic: bool | int = True,
-        radius: int = 16, sample_mode: SampleMode = SampleMode.SQUARE
-    ) -> vs.VideoNode:
-        """
-        Add f3kdb grain to the clip.
-
-        :param clip:            Source clip
-        :param grains:          Specifies amount of grains added in the last debanding stage.
-
-        :return:                Grained clip
-        """
-
-        radius = fallback(self.radius, radius)
-
-        sample_mode, _ = self.plugin.get_sample_step(fallback(self.sample_mode, sample_mode), self.__class__.deband)
-
-        gry, grc = normalize_seq(fallback(self.grains, strength), 2)
-
-        return self.plugin.Deband(  # type: ignore[call-overload,no-any-return]
-            clip, 1, 1, 1, grainy=gry, grainc=grc, range=radius, sample_mode=sample_mode,
-            dynamic_grain=fallback(self.dynamic_grain, dynamic)
-        )
+        return func.return_clip(debanded)
