b4e487ed0c63c5529bdd7ef2fbf3d080eacfee59
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-04-02 22:04:59 +0200

    Rewrite deband_detail_mask

--- vsdeband/mask.py
+++ vsdeband/mask.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from vsexprtools import ExprOp, combine
-from vsmasktools import Morpho, retinex
+from vsexprtools import ExprOp
+from vsmasktools import Morpho, Prewitt, retinex
 from vsrgtools import RemoveGrainMode, RemoveGrainModeT, gauss_blur, removegrain
-from vstools import core, depth, expect_bits, get_y, iterate, vs
+from vstools import get_y, vs
 
 __all__ = [
     'deband_detail_mask'
@@ -11,25 +11,27 @@ __all__ = [
 
 
 def deband_detail_mask(
-    clip: vs.VideoNode,
-    sigma: float = 1.0, rxsigma: list[int] = [50, 200, 350],
-    pf_sigma: float | None = 1.0, brz: tuple[int, int] = (2500, 4500),
+    clip: vs.VideoNode, sigma: float = 1.0, rxsigma: list[int] = [50, 200, 350],
+    pf_sigma: float | None = 1.0, brz: tuple[float, float] = (0.38, 0.68),
     rg_mode: RemoveGrainModeT = RemoveGrainMode.MINMAX_MEDIAN_OPP
 ) -> vs.VideoNode:
-    clip_y, bits = expect_bits(get_y(clip), 16)
+    clip_y = get_y(clip)
 
-    pf = gauss_blur(clip_y, pf_sigma) if pf_sigma else clip_y
-    ret = retinex(pf, rxsigma, upper_thr=0.005)
+    ret = retinex(
+        clip_y if pf_sigma is None else gauss_blur(clip_y, pf_sigma), rxsigma, upper_thr=0.005
+    )
+
+    brz0, brz1 = (br / 10 for br in brz)
 
     blur_ret = gauss_blur(ret, sigma)
-    blur_ret_diff = combine([blur_ret, ret], ExprOp.SUB).std.Deflate()
-    blur_ret_brz = iterate(blur_ret_diff, core.std.Inflate, 4)
-    blur_ret_brz = Morpho.closing(blur_ret_brz.std.Binarize(brz[0]), coords=8)
+    blur_ret_diff = Morpho.deflate(ExprOp.SUB(blur_ret, ret))
 
-    prewitt_mask = clip_y.std.Prewitt().std.Binarize(brz[1]).std.Deflate().std.Inflate()
-    prewitt_brz = Morpho.closing(prewitt_mask.std.Binarize(brz[1]), coords=4)
+    blur_ret_brz = Morpho.inflate(blur_ret_diff, 4)
+    blur_ret_brz = Morpho.closing(Morpho.binarize(blur_ret_brz, brz0), coords=8)
 
-    merged = combine([blur_ret_brz, prewitt_brz], ExprOp.ADD)
-    rm_grain = removegrain(merged, rg_mode)
+    prewitt_mask = Morpho.inflate(Morpho.deflate(Prewitt.edgemask(clip_y, brz1, brz1)))
+    prewitt_brz = Morpho.closing(Morpho.binarize(prewitt_mask, brz1), coords=4)
 
-    return depth(rm_grain, bits)
+    merged = ExprOp.ADD(blur_ret_brz, prewitt_brz)
+
+    return removegrain(merged, rg_mode)
